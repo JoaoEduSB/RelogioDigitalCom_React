ea2f04f1b2f630dfbec928f4e1bc8bba88ee0f5c
João Eduardo <joao.eduardo.souza@hotmail.com>
2024-05-17 17:32:51 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'; 
+
+import RelogioDigital from './components/Relogio'; 
+
+  
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <RelogioDigital /> 
+
+    </div> 
+
+  ); 
+
+} 
+
+  
+
+export default App; 
